1126dbb6338ca99e86fea473185ba0547e680a7a
microbit-carlos <carlos@microbit.org>
2021-02-09 08:56:31 +0000

    Fix small typo.

--- examples/header.ts
+++ examples/header.ts
@@ -1,4 +1,4 @@
 /**
- * (c) star_year-current_year, Micro:bit Educational Foundation and contributors.
+ * (c) start_year-current_year, Micro:bit Educational Foundation and contributors.
  * SPDX-License-Identifier: <license-id>
  */
